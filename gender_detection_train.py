6d63d5ae9cb1eb7de3c73880b68a8b2023664cbf
Valentin Gouzy <61479371+gouzylla@users.noreply.github.com>
2022-09-26 15:19:20 +0200

    Update gender_detection_train.py

--- gender_detection_train.py
+++ gender_detection_train.py
@@ -4,7 +4,6 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.utils import to_categorical, plot_model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import BatchNormalization, Conv2D, MaxPooling2D, Activation, Flatten, Dropout, Dense
-from tensorflow.keras import backend as K
 from tensorflow.keras import callbacks
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
